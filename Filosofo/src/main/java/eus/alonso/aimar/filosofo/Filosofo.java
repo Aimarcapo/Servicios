76cb6765aa628a5e24f05d0d125b6da1518124da
Aimar Alonso <alonso.aimar@uni.eus>
2023-10-25 09:58:07 +0200

    25/10/2023

---
+++ Filosofo/src/main/java/eus/alonso/aimar/filosofo/Filosofo.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package eus.alonso.aimar.filosofo;
+
+import java.util.concurrent.Semaphore;
+
+/**
+ *
+ * @author aimar
+ */
+public class Filosofo {
+
+    public static void main(String[] args) {
+        Semaphore s1=new Semaphore(1);
+    }
+}
